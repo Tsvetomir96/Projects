675180158f90be4dd57273fce7b6afda0b00875b
Tsvetomir96 <59999160+Tsvetomir96@users.noreply.github.com>
2020-01-17 13:40:11 +0200

    Еxam Fundamentals

---
+++ Mid Exam/Technology Fundamentals Mid Exam - 10 March 2019 Group 1/01 Spring Vacation Trip/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _01_Spring_Vacation_Trip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysOfTrip = int.Parse(Console.ReadLine());
+            double budget = double.Parse(Console.ReadLine());
+            int groupOfPeople = int.Parse(Console.ReadLine());
+            double fuelPerKilometer = double.Parse(Console.ReadLine());
+            double foodExpenses = double.Parse(Console.ReadLine());
+            double priceForNight = double.Parse(Console.ReadLine());
+
+            if (groupOfPeople > 10)
+            {
+                priceForNight *= 0.75;
+            }
+
+            double expenses = (foodExpenses + priceForNight) * groupOfPeople * daysOfTrip;
+
+            for (int i = 1; i <= daysOfTrip; i++)
+            {
+                double travelDistance = double.Parse(Console.ReadLine());
+
+                expenses += travelDistance * fuelPerKilometer;
+
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    expenses *= 1.4;
+                }
+                if (i % 7 == 0)
+                {
+                    expenses -= expenses / groupOfPeople;
+                }
+
+                if (expenses > budget)
+                {
+                    Console.WriteLine($"Not enough money to continue the trip. You need {(expenses - budget):f2}$ more.");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"You have reached the destination. You have {(budget - expenses):f2}$ budget left.");
+
+
+        }
+    }
+}
---
+++ Mid Exam/Technology Fundamentals Mid Exam - 10 March 2019 Group 1/02 Hello France/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_Hello_France
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split("|");
+            double budget = double.Parse(Console.ReadLine());
+
+            var finalPrice = new List<double>();
+            double profit = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string[] splitProducts = input[i].Split("->");
+
+                string product = splitProducts[0];
+                double price = double.Parse(splitProducts[1]);
+
+                if (product == "Clothes")
+                {
+                    if (price <= 50.00 && budget >= price)
+                    {
+                        budget -= price;
+                        profit += price * 0.40;
+                        finalPrice.Add(price * 1.40);
+                    }
+                }
+                else if (product == "Shoes")
+                {
+                    if (price <= 35.00 && budget >= price)
+                    {
+                        budget -= price;
+                        profit += price * 0.40;
+                        finalPrice.Add(price * 1.40);
+                    }
+                }
+                else if (product == "Accessories")
+                {
+                    if (price <= 20.50 && budget >= price)
+                    {
+                        budget -= price;
+                        profit += price * 0.40;
+                        finalPrice.Add(price * 1.40);
+                    }
+                }
+            }
+
+            foreach (var number in finalPrice)
+            {
+                Console.Write($"{number:F2}" + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Profit: {profit:F2}");
+
+            if (budget + finalPrice.Sum() >= 150)
+            {
+                Console.WriteLine("Hello, France!");
+            }
+            else
+            {
+                Console.WriteLine("Time to go.");
+            }
+        }
+    }
+}
